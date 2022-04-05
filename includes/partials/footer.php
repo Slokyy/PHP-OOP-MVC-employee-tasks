e1eefac138be7ce33fce2589eb6ad0659bd3a93a
Slokyy <slobodancvetkovic995@gmail.com>
2022-04-05 15:26:54 +0200

    ITS MACROOOOO

--- includes/partials/footer.php
+++ includes/partials/footer.php
@@ -1,6 +1,6 @@
 <footer class="footer flex flex-column flex-center">
-  <div>Opposite of MicroSoft, so we are just better</div>
-  <div>Copyright &copy; 2022 MicroHard</div>
+  <div>Opposite of McroSoft, so we are just better</div>
+  <div>Copyright &copy; 2022 MacroHard</div>
 </footer>
 </body>
 </html>
